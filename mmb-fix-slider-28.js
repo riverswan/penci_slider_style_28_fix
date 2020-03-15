29251349f23e420ba1b9b157355cabcc051707b4
Pavel Lebedev <maxwinrate@gmail.com>
2020-03-15 16:22:30 +0200

    Init

---
+++ mmb-fix-slider-28.js
@@ -0,0 +1,22 @@
+(function ($) {
+    'use strict';
+    function changeSliderCss(){
+        if ($(window).width() < 767) {
+            $('.featured-style-28  .owl-stage').css({
+                'right': ($(window).width() - $('.owl-item').width()) / -2 + "px",
+            })
+        }
+    }
+
+    $(window).load(function () {
+        changeSliderCss();
+    });
+
+    $(window).resize(function () {
+        changeSliderCss()
+    })
+
+})(jQuery);
+
+
+// return ( $(window).width() - parseInt( $('.owl-item').width() ) ) / -2 ;
