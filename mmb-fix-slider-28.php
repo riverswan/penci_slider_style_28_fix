519e02514f89acbae8c1e568f61e66ccd9d699e6
Pavel Lebedev <maxwinrate@gmail.com>
2020-04-09 22:37:18 +0300

    Language adaptation

--- mmb-fix-slider-28.php
+++ mmb-fix-slider-28.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Soledad Slider 28 fix
  * Plugin URI:        https://github.com/riverswan/penci_slider_style_28_fix
- * Description:       Quick fix for Style №28 homepage slider of Soledad Wordpress theme.
+ * Description:       Quick fix for Style №28 homepage slider of Soledad WordPress theme.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
@@ -15,7 +15,19 @@
  * Domain Path:       /languages
  */
 
-add_action('wp_enqueue_scripts', function () {
-	wp_enqueue_script('mmb-fix-slider-28', plugin_dir_url(__FILE__) . 'mmb-fix-slider-28.js',array( 'jquery' ),'',true);
-});
 
+if ( is_page( 'tests' ) ) {
+	add_action(
+		'wp_enqueue_scripts',
+		function () {
+			wp_enqueue_script( 'mmb-fix-slider-28', plugin_dir_url( __FILE__ ) . 'mmb-fix-slider-28_for_tests_page.js', array( 'jquery' ), '', true );
+		}
+	);
+} else {
+	add_action(
+		'wp_enqueue_scripts',
+		function () {
+			wp_enqueue_script( 'mmb-fix-slider-28', plugin_dir_url( __FILE__ ) . 'mmb-fix-slider-28.js', array( 'jquery' ), '', true );
+		}
+	);
+}
